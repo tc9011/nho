8908ad2705f7437d88e75f654bf42ec3c7950c72
tc9011 <tc9011@126.com>
2019-04-14 14:24:19 +0800

    feat(lint): add logout

--- src/app/pages/camp-list/camp-list.component.ts
+++ src/app/pages/camp-list/camp-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StorageService } from '../../core/storage.service';
+import { HttpService } from '../../core/http.service';
 
 @Component({
   selector: 'app-camp-list',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampListComponent implements OnInit {
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private storageService: StorageService,
+    private httpService: HttpService
+  ) {
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.httpService.getData(`/logout`).subscribe(
+      () => {
+        this.storageService.removeSessionStorage('token');
+        this.router.navigate(['/login']);
+      },
+    );
   }
 }
